7cd5361c10c4a46f7d5ef51ce9ef4007a2b4b576
IB-315168 <315168@via.dk>
2023-08-20 21:33:36 +0200

    Change picker and extract EXIF data

--- app/src/main/java/com/ib315168/exifmap/MainActivity.java
+++ app/src/main/java/com/ib315168/exifmap/MainActivity.java
@@ -1,15 +1,13 @@
 package com.ib315168.exifmap;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.PickVisualMediaRequest;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.exifinterface.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
@@ -17,7 +15,6 @@ import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -31,34 +28,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = (Button) findViewById(R.id.button);
-        ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
+        /*  Previous approach
+            ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
                 registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
                     // Callback is invoked after the user selects a media item or closes the
                     // photo picker.
                     if (uri != null) {
                         Log.d("PhotoPicker", "Selected URI: " + uri);
-
-                        try {
-                            selectedImage = getContentResolver().openInputStream(uri);
-                            metadata = ImageMetadataReader.readMetadata(selectedImage);
-                            for (Directory directory : metadata.getDirectories()) {
-                                for (Tag tag : directory.getTags()) {
-                                    System.out.println(tag);
-                                }
-                            }
-                        } catch (ImageProcessingException | IOException e) {
-                            throw new RuntimeException(e);
-                        }
+                        processSelectedImage(uri);
                     } else {
                         Log.d("PhotoPicker", "No media selected");
                     }
                 });
+         */
+
+        final ActivityResultLauncher<String> pickImageLauncher = registerForActivityResult(
+                new ActivityResultContracts.GetContent(),
+                uri -> {
+                    if (uri != null) {
+                        processSelectedImage(uri);
+                    } else {
+                        // Handle case when no image was selected
+                    }
+                }
+        );
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                pickMedia.launch(new PickVisualMediaRequest.Builder()
-                        .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
-                        .build());
+                /* Previous approach
+                    pickMedia.launch(new PickVisualMediaRequest.Builder()
+                            .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+                            .build());
+                */
+                pickImageLauncher.launch("image/*");
             }
         });
+    }
+
+    private void processSelectedImage(Uri uri) {
+        try {
+            selectedImage = getContentResolver().openInputStream(uri);
+            metadata = ImageMetadataReader.readMetadata(selectedImage);
+
+            for (Directory directory : metadata.getDirectories()) {
+                for (Tag tag : directory.getTags()) {
+                    System.out.println(tag);
+                }
+            }
+
+            selectedImage.close();
+        } catch (ImageProcessingException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
