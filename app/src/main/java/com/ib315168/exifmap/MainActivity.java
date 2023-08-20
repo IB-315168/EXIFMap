e7830ec33ffbdd132c6010f857dcc04959621ec8
IB-315168 <315168@via.dk>
2023-08-20 11:58:54 +0200

    Add EXIF tags reading

--- app/src/main/java/com/ib315168/exifmap/MainActivity.java
+++ app/src/main/java/com/ib315168/exifmap/MainActivity.java
@@ -5,12 +5,25 @@ import androidx.activity.result.PickVisualMediaRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.exifinterface.media.ExifInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
+    InputStream selectedImage;
+    Metadata metadata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,18 @@ public class MainActivity extends AppCompatActivity {
                     // photo picker.
                     if (uri != null) {
                         Log.d("PhotoPicker", "Selected URI: " + uri);
+
+                        try {
+                            selectedImage = getContentResolver().openInputStream(uri);
+                            metadata = ImageMetadataReader.readMetadata(selectedImage);
+                            for (Directory directory : metadata.getDirectories()) {
+                                for (Tag tag : directory.getTags()) {
+                                    System.out.println(tag);
+                                }
+                            }
+                        } catch (ImageProcessingException | IOException e) {
+                            throw new RuntimeException(e);
+                        }
                     } else {
                         Log.d("PhotoPicker", "No media selected");
                     }
